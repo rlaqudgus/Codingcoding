5db0fc034c8e8ff9cf1c865f5df72f41d4767bf9
Administrator <Administrator@LAPTOP-VP0MU2P5>
2022-08-01 09:12:35 +0900

    초기 커밋.

---
+++ Codingcoding/Program.cs
@@ -0,0 +1,29 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        string number = Console.ReadLine();
+        int n = int.Parse(number);        
+        
+        long a = 1;
+        long sum=0;
+        long sumsum;
+
+        if (number.Length != 0)
+        {
+            for (int i = 1; i < 20; i++)
+            {
+                sum = 10 * sum;
+                sumsum = sum + a;
+                sum = sumsum;
+
+                if (sumsum % n == 0)
+                {
+                    Console.WriteLine(sumsum.ToString().Length);
+                    break;
+                }
+            }
+        }
+    }
+}
+
