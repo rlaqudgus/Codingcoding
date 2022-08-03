90720f2bdf1667bcc2854923e6d60b879bf26df6
Byeong Hyeon Kim <kbh397241@gmail.com>
2022-08-03 11:02:33 +0900

    백준 1110부터 10871 거의 완료

---
+++ Codingcoding/Baekjoon/Bj_1110-10871.cs
@@ -0,0 +1,86 @@
+﻿using System.Text;
+
+class Bj_111010871
+{
+    public static void Main()
+    {
+        //10871
+        //    string[] NnX = Console.ReadLine().Split(" ");
+
+        //    int N = int.Parse(NnX[0]);
+        //    int X = int.Parse(NnX[1]);
+
+        //    string[] array = new string[N];
+        //    array = Console.ReadLine().Split(" ");
+
+        //    foreach (var number in array)
+        //    {
+        //        int num = int.Parse(number);
+        //        if (num<X)
+        //        {
+        //            Console.Write($"{num} ");
+        //        }
+        //    }
+
+        //2439
+        //int starNum = int.Parse(Console.ReadLine());
+        //for (int i = 1; i <= starNum; i++)
+        //{
+        //    string star = new string('*', i);
+        //    Console.WriteLine(star.PadLeft(starNum));
+        //}
+
+        //11022
+        //Input
+        //int howMany = int.Parse(Console.ReadLine());
+        //int[] array = new int[howMany * 2];
+
+        //for (int i = 0; i < howMany; i++)
+        //{
+        //    string[] numArray = Console.ReadLine().Split(" ");
+        //    int a = int.Parse(numArray[0]);
+        //    int b = int.Parse(numArray[1]);
+        //    array[i * 2] = a;
+        //    array[i * 2 + 1] = b;
+        //}
+        ////Output
+        //for (int i = 1; i <= howMany; i++)
+        //{
+        //    Console.WriteLine($"Case #{i}: {array[i*2-2]} + {array[i*2-1]} = {array[i * 2 - 2] + array[i*2-1]}");
+        //}
+
+        //2742
+        //int N = int.Parse(Console.ReadLine());
+        //StringBuilder answer = new StringBuilder();
+        //for (int i = N; i > 0; i--)
+        //{
+        //    answer.AppendLine(i.ToString());
+        //}
+        //Console.WriteLine(answer);
+
+        //1110
+        //Input
+        int cycleStart = int.Parse(Console.ReadLine());
+
+        int count = 0;
+        int cycleEnd = 0;
+
+        while (cycleStart != cycleEnd)
+        {
+            int ten = cycleStart / 10;
+            int one = cycleStart % 10;
+
+            if (ten+one>=10)
+            {
+                int newone = (ten + one) % 10;
+                cycleEnd = 10 * one + newone;
+            }
+
+            cycleEnd = 10 * one + ten + one;
+            count++;
+            Console.WriteLine("반복");
+        }
+
+        Console.WriteLine(count);
+    }
+}
--- Codingcoding/Baekjoon/Bj_2525.cs
+++ Codingcoding/Baekjoon/Bj_2525.cs
@@ -1,33 +1,45 @@
 ﻿class Bj_2525
 {
-    public static void Main()
+    public static void Mai()
     {
-        string[] hourAndMinute = Console.ReadLine().Split(" ");
-        int hour = int.Parse(hourAndMinute[0]);
-        int minute = int.Parse(hourAndMinute[1]);
+        //string[] hourAndMinute = Console.ReadLine().Split(" ");
+        //int hour = int.Parse(hourAndMinute[0]);
+        //int minute = int.Parse(hourAndMinute[1]);
 
-        int time = int.Parse(Console.ReadLine());
+        //int time = int.Parse(Console.ReadLine());
 
-        int endHour = hour;
-        int endMinute;
+        //int endHour = hour;
+        //int endMinute;
 
-        int plusHour = time / 60;
-        int plusMinute = time % 60;
+        //int plusHour = time / 60;
+        //int plusMinute = time % 60;
 
-        endMinute = minute + time;
+        //endMinute = minute + time;
 
-        if (endMinute >= 60)
-        {
-            
-            endMinute = plusMinute;
-            endHour = hour + plusHour;
+        //if (endMinute >= 60)
+        //{
 
-            if (endHour>23)
-            {
-                endHour = endHour - 24;
-            }
-        }
+        //    endMinute = plusMinute;
+        //    endHour = hour + plusHour;
 
-        Console.WriteLine(endHour + " " + endMinute);
+        //    if (endHour>23)
+        //    {
+        //        endHour = endHour - 24;
+        //    }
+        //}
+
+        //Console.WriteLine(endHour + " " + endMinute);
+        Console.WriteLine("사각형의 너비를 입력하세요");
+        string width = Console.ReadLine();
+        Console.WriteLine("사각형의 높이를 입력하세요");
+        string height = Console.ReadLine();
+
+        int w = int.Parse(width);
+        int h = int.Parse(height);
+
+        Console.WriteLine($"사각형의 넓이는:{w*h}");
+        float a = 0.1f;
+        double b = 0.01;
+        Console.WriteLine(a*b);
     }
 }
